2af3b62efdeb7509b769820de969263835814c67
wenwenwenwu <wenwenwenwu@foxmail.com>
2018-02-27 15:30:53 +0800

    抢单去掉搜索框

--- XiaoBie/XiaoBie/Order（抢单）/主页面/GrabViewController.swift
+++ XiaoBie/XiaoBie/Order（抢单）/主页面/GrabViewController.swift
@@ -13,19 +13,13 @@ class GrabViewController: UIViewController, UIScrollViewDelegate {
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(searchButton)
         view.addSubview(selectView)
         view.addSubview(pageView)
+        setupNavigationBar()
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.isNavigationBarHidden = true
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.isNavigationBarHidden = false
+    //MARK: - Setup
+    func setupNavigationBar() {
+        navigationItem.title = "抢单"
     }
     
     //MARK: - Private Method
@@ -37,32 +31,8 @@ class GrabViewController: UIViewController, UIScrollViewDelegate {
         selectView.currentIndex = currentIndex
     }
 
-    
-    //MARK: - Event Response
-    @objc func searchButtonAction() {
-        print("搜索")
-    }
-    
     //MARK: - Lazyload
-    lazy var searchButton: UIButton = {
-        let button = UIButton.init(type: .custom)
-        button.frame = CGRect.init(x: 13, y: 26, width: screenWidth-13*2, height: 34)
-        button.adjustsImageWhenHighlighted = false
-        button.setBackgroundImage(gray_F5F5F5.colorImage(), for: .normal)
-        button.contentHorizontalAlignment = .left
-        button.titleEdgeInsets = UIEdgeInsets.init(top: 0, left: 15, bottom: 0, right: 0)
-        button.titleLabel?.font = font16
-        button.setTitle("搜索", for: .normal)
-        button.setTitleColor(gray_B3B3B3, for: .normal)
-        button.setImage(#imageLiteral(resourceName: "icon_search"), for: .normal)
-        button.imageEdgeInsets = UIEdgeInsets.init(top: 0, left: 8, bottom: 0, right: 0)
-        button.layer.cornerRadius = 2
-        button.clipsToBounds = true
-        button.addTarget(self, action: #selector(searchButtonAction), for: .touchUpInside)
-        return button
-    }()
-    
-    lazy var selectView = SelectView.viewWith(frame: CGRect.init(x: 60, y: searchButton.bottom, width: screenWidth-60*2, height: 44), titleArray:  ["全部", "送手机", "送宽带"], sliderWidth: 35) { [weak self] (currentIndex) in
+    lazy var selectView = SelectView.viewWith(frame: CGRect.init(x: 60, y: 0, width: screenWidth-60*2, height: 44), titleArray:  ["全部", "送手机", "送宽带"], sliderWidth: 35) { [weak self] (currentIndex) in
         self?.selectViewChangeCurrentIndex(currentIndex: currentIndex)
     }
     
@@ -70,7 +40,7 @@ class GrabViewController: UIViewController, UIScrollViewDelegate {
     lazy var allVC = GrabListViewController.controllerWith(listType: .all)
     lazy var phoneVC = GrabListViewController.controllerWith(listType: .phone)
     lazy var webVC = GrabListViewController.controllerWith(listType: .web)
-    lazy var pageView = PageView.viewWith(ownerVC: self, frame: CGRect.init(x: 0, y: selectView.bottom, width: screenWidth, height: screenHeight-selectView.bottom-tabbarHeight), VCArray: [allVC, phoneVC, webVC]) { [weak self] (currentIndex) in
+    lazy var pageView = PageView.viewWith(ownerVC: self, frame: CGRect.init(x: 0, y: selectView.bottom, width: screenWidth, height: screenHeight-selectView.bottom-tabbarHeight-navigationBarHeight), VCArray: [allVC, phoneVC, webVC]) { [weak self] (currentIndex) in
         self?.pageViewChangeCurrentIndex(currentIndex: currentIndex)
     }
 }
