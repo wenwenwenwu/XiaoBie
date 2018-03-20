47608b12f58255e66fcbf78f29df33b661cc12bc
wenwenwenwu <wenwenwenwu@foxmail.com>
2018-03-20 15:44:31 +0800

    管理员位置页面完成

---
+++ XiaoBie/XiaoBie/M（管理员）/Home（首页）/位置/MLocationModel.swift
@@ -0,0 +1,50 @@
+//
+//  MLocationModel.swift
+//  XiaoBie
+//
+//  Created by wuwenwen on 2018/3/20.
+//  Copyright © 2018年 wenwenwenwu. All rights reserved.
+//
+
+import UIKit
+import YYModel
+
+class MLocationListResponseModel: NSObject,YYModel {
+    
+    @objc var code = ""
+    @objc var msg = ""
+    @objc var result = ""
+    @objc var data = [MLocationItemModel]()
+    
+    class func parse(dict : Any ) -> MLocationListResponseModel{
+        let model = MLocationListResponseModel.yy_model(withJSON: dict)
+        return model!
+    }
+    
+    static func modelContainerPropertyGenericClass() -> [String : Any]? {
+        return ["data":MLocationItemModel.self]
+    }
+}
+
+class MLocationItemModel: NSObject {
+    
+    @objc var id = ""
+    @objc var name = ""
+    @objc var phone = ""
+    @objc var avatar = ""
+    @objc var role = ""
+    @objc var role_level = ""
+    @objc var business_type = ""
+    @objc var agent_id = ""
+    @objc var agent_name = ""
+    @objc var lock_order_id = ""
+    @objc var create_time = ""
+    @objc var update_time = ""
+    @objc var source_id = ""
+    @objc var latitude = ""
+    @objc var longitude = ""
+    @objc var address = ""
+    @objc var password = ""
+    @objc var locate_time = ""
+}
+
---
+++ XiaoBie/XiaoBie/M（管理员）/Home（首页）/位置/MLocationView.swift
@@ -0,0 +1,118 @@
+//
+//  MLocationCell.swift
+//  XiaoBie
+//
+//  Created by wuwenwen on 2018/3/20.
+//  Copyright © 2018年 wenwenwenwu. All rights reserved.
+//
+
+import UIKit
+
+class MLocationCell: UITableViewCell {
+    
+    //MARK: - FactoryMethod
+    class func cellWith(tableView : UITableView) -> MLocationCell{
+        let reuseIdentifier = "locationCell";
+        var cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier)
+        if (cell == nil) {
+            cell = MLocationCell(style: .default, reuseIdentifier: reuseIdentifier)
+        }
+        return cell as! MLocationCell
+    }
+    
+    //MARK: - Init
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = .none
+        contentView.addSubview(roundLabel)
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(locationImageView)
+        contentView.addSubview(addressLabel)
+        contentView.addSubview(lineView)
+        contentView.backgroundColor = white_FFFFFF
+        setupFrame()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Setup
+    func setupFrame() {
+        roundLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(15)
+            make.left.equalTo(14)
+            make.width.height.equalTo(40)
+        }
+        
+        nameLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(15)
+            make.left.equalTo(roundLabel.snp.right).offset(11)
+            make.height.equalTo(15)
+        }
+        
+        locationImageView.snp.makeConstraints { (make) in
+            make.top.equalTo(nameLabel.snp.bottom).offset(12)
+            make.left.equalTo(roundLabel.snp.right).offset(11)
+            make.width.equalTo(12)
+            make.height.equalTo(15)
+        }
+        
+        addressLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(40)
+            make.left.equalTo(locationImageView.snp.right).offset(5)
+            make.right.equalTo(-14)
+            make.bottom.equalTo(-14)
+        }
+        
+        lineView.snp.makeConstraints { (make) in
+            make.left.bottom.right.equalToSuperview()
+            make.height.equalTo(1)
+        }
+    }
+    
+    //MARK: - Properties
+    lazy var roundLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = font14
+        label.textColor = white_FFFFFF
+        label.backgroundColor = blue_3296FA
+        label.layer.cornerRadius = 20
+        label.clipsToBounds = true
+        return label
+    }()
+    
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = font16Medium
+        label.textColor = black_333333
+        return label
+    }()
+    
+    lazy var locationImageView = UIImageView.init(image: #imageLiteral(resourceName: "icon_dw"))
+    
+    lazy var addressLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = font14
+        label.textColor = gray_666666
+        return label
+    }()
+    
+    lazy var lineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = gray_F0F0F0
+        return view
+    }()
+    
+    var model = MLocationItemModel() {
+        didSet {
+            nameLabel.text = model.name
+            addressLabel.text = model.address
+            //roundLabel
+            let name = model.name
+            roundLabel.text = String(name[name.startIndex])            
+        }
+    }
+}
