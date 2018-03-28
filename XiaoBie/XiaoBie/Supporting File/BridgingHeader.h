e85c74be2bf1c341a767478285096b591896cb0b
wenwenwenwu <wenwenwenwu@foxmail.com>
2018-03-28 17:46:16 +0800

    基本完成

---
+++ XiaoBie/XiaoBie/Supporting File/BridgingHeader.h
@@ -0,0 +1,22 @@
+//
+//  BridgingHeader.h
+//  XiaoBie
+//
+//  Created by wuwenwen on 2018/2/5.
+//  Copyright © 2018年 wenwenwenwu. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+//高德地图
+#import <AMapFoundationKit/AMapFoundationKit.h>
+#import <AMapLocationKit/AMapLocationKit.h>
+//云信
+#import "NIMKit.h"
+//极光推送
+#import <JPUSHService.h>
+#ifdef NSFoundationVersionNumber_iOS_9_x_Max
+#import <UserNotifications/UserNotifications.h>
+#endif
+
+#endif /* BridgingHeader_h */
